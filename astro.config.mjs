b65d6a04f749ee4be544afee053da474eea75e10
Daveed <daveed@santocabron.com>
2025-09-25 13:41:36 -0400

    including tailwind, block content component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sell.markket.place',
   // Remove base path since you're using a custom domain
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
